96ffb8fb86522ff394f49198a82e84b912672fab
Peter Cheng <teed7334@gmail.com>
2017-03-27 21:24:38 +0800

    1.追加自我學習機制

--- js/classes/annotations.js
+++ js/classes/annotations.js
@@ -19,23 +19,45 @@ Annotations.prototype.generatorAnnotations = function(name, json, io) {
     for (i in json) {
         type = typeof(json[i]);
 
-        if ('number' === type) {
+        if ('number' === type) { /** 當屬性形態為number時，轉為int **/
             type = 'int';
         }
 
-        if ('object' !== type && 0 > jQuery.inArray(i, deny1)) {
-            code += ' * ' + title + ' ' + type + ' $' + name + '->' + i + '\n';
+        if ('object' !== type && 0 > jQuery.inArray(i, deny1)) { /** 當屬性不為object時，且不為重覆之屬性名時 **/
+
+            description = localStorage.getItem('@' + i.toLowerCase());
+            description = JSON.parse(description);
+            description = 'undefined' !== typeof(description.description) ? description.description : '';
+
+            if(isNaN(i)) {
+                code += ' * ' + title + ' ' + type + ' $' + name + '->' + i + ' ' + description + '\n';
+            } else {
+                code += ' * ' + title + ' ' + type + ' $' + name + "['" + i + "']" + ' ' + description + "\n";
+            }
             deny1.push(i);
         } else {
             for (j in json[i]) {
                 type = typeof(json[i][j]);
 
-                if ('number' === type) {
+                if ('number' === type) { /** 當屬性形態為number時，轉為int **/
                     type = 'int';
                 }
 
                 if ('object' !== type && 0 > jQuery.inArray(j, deny2)) {
-                    code += ' * ' + title + ' ' + type + ' $' + name + '[' + i + ']->' + j + '\n';
+
+                    description = localStorage.getItem('@' + j.toLowerCase());
+                    description = JSON.parse(description);
+                    description = 'undefined' !== typeof(description.description) ? description.description : '';
+
+                    if(isNaN(i) && isNaN(j)) { /** 當第一層與第二層皆為屬性時 **/
+                        code += ' * ' + title + ' ' + type + ' $' + name + '->' + i + '->' + j + ' ' + description + ';\n';
+                    } else if(!isNaN(i) && isNaN(j)) { /** 當第一層為陣列與第二層為屬性時 **/
+                        code += ' * ' + title + ' ' + type + ' $' + name + "['" + i + "']->" + j + ' ' + description + ';\n';
+                    } else if(isNaN(i) && !isNaN(j)) { /** 當第一層為屬性與第二層皆為陣列時 **/
+                        code += ' * ' + title + ' ' + type + ' $' + name + '->' + i + "['" + j + "'];" + ' ' + description + "\n";
+                    } else if(!isNaN(i) && !isNaN(j)) { /** 當第一層與第二層皆為陣列時 **/
+                        code += ' * ' + title + ' ' + type + ' $' + name + "['" + i + "']['" + j + "'];" + ' ' + description + "\n";
+                    }
                     deny2.push(j);
                 }
             }
